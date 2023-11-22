7579c5dfd562b763efdc776aef2fb6c49aa6e253
Keadon Mitchell <mitchell.keadon@outlook.com>
2023-11-22 15:32:34 -0600

    Add dynamic font awesome support

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid, brands } from '@fortawesome/fontawesome-svg-core/import.macro';
+
 function App() {
   return (
     <>
       <a className="name" href="https://github.com/KeadonM" target="blank">
-        Keadon Mitchell
+        Keadon Mitchell <FontAwesomeIcon icon={brands('github')} />
       </a>
+
+      <div className="name">
+        Under Construction <FontAwesomeIcon icon={solid('Wrench')} />
+      </div>
     </>
   );
 }
