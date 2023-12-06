4852ba09a18b70ef23bc047f9ef301e29ef67911
Keadon Mitchell <mitchell.keadon@outlook.com>
2023-12-06 01:05:41 -0600

    add under construction

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-zinc-800 p-24 ">
-      <div className="text-5xl text-indigo-500">Keadon Mitchell</div>
+      <div className="flex flex-col items-center">
+        <div className="text-5xl text-indigo-500">Keadon Mitchell</div>
+        <div className="text-4xl text-white">Under Construction</div>
+      </div>
     </main>
   );
 }
