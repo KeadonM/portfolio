57c24b58f2aca8584e54df943f1c1474ce6681fa
Keadon Mitchell <mitchell.keadon@outlook.com>
2023-12-06 00:54:14 -0600

    setup next

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-zinc-800 p-24 ">
+      <div className="text-5xl text-indigo-500">Keadon Mitchell</div>
+    </main>
+  );
+}
